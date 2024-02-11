a34da26ce9467cf8c279cad7a3005e3d063d5fa3
KultidGames <chrisl0078@gmail.com>
2024-02-11 16:33:01 -0500

    VoxelShape

--- src/main/java/net/lambdacomplex/mayflowersdelight/block/custom/DryingTableBlock.java
+++ src/main/java/net/lambdacomplex/mayflowersdelight/block/custom/DryingTableBlock.java
@@ -2,31 +2,61 @@ package net.lambdacomplex.mayflowersdelight.block.custom;
 
 import net.lambdacomplex.mayflowersdelight.block.entity.DryingTableBlockEntity;
 import net.lambdacomplex.mayflowersdelight.block.entity.ModBlockEntities;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class DryingTableBlock extends BaseEntityBlock{
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D);
     protected DryingTableBlock(Properties prop) {
         
         super(prop);
     }
+
+    @Override
+    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        return SHAPE;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.rotate(pMirror.getRotation(pState.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
 
 
     /* Block Entity */
@@ -58,8 +88,7 @@ public class DryingTableBlock extends BaseEntityBlock{
                 throw new IllegalStateException("Our Container provider is missing!");
             }
         }
-        //DEBUGGER
-        Minecraft.getInstance().player.addItem(new ItemStack(Items.ACACIA_LEAVES, 1));
+
         return InteractionResult.sidedSuccess(pLevel.isClientSide());
     }
 
